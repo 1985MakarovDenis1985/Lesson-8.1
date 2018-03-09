8afc3c6a5a962a745c8ca4d636067a625f8b2fd5
Denis, Makarov <makarovdenis07081985@gmail.com>
2018-03-09 16:25:55 +0200

    add lesson 8 with bootstrap

--- Lesson_8.1/js/common.js
+++
@@ -1,8 +0,0 @@
-$('.slick-slider').slick({
-
-	prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-angle-left"></i></button>',
-	nextArrow: '<button type="button" class="slick-next"><i class="fas fa-angle-right"></i></button>',
-
-	dots: true,
-
-})
